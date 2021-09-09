4b178f251806dbca8dc60791c4dcbad147597d5c
JC Nolan <jc@jcnolan.com>
2021-09-09 08:34:25 -0700

    Initial Commit

---
+++ CellMergingAndTwiddlesInNSTableView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CellMergingAndTwiddlesInNSTableView
+//
+//  Created by JC Nolan on 9/9/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
